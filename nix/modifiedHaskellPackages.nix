6a244b12be3e19908912f6750a130d22a953f33e
Andrew McCluskey <andrew@ajmccluskey.com>
2018-05-03 10:42:41 +1000

    Get building on 18.03 + refactor nix stuff
    - Pin nixpkgs to the 17.09 release by default, as a bunch of packages fall
      outside acceptable bounds in 18.03 and I don't want to deal with that yet.
    - Use `callHackage` instead of `callPackage` on nix files we manage ourselves
    - Disable tests on servant-auth-client as they were failing when trying to build
      with GHC 8.2.2
    - Don't version check servant when choosing servant-auth-client versions -- just
      use the one that we need (17.09 and 18.03 both have servant-0.11).
    - Jailbreak concurrent-output on anything older than GHC 8.2 as the version of
      process is too old to satisfy dependency checks.

--- nix/modifiedHaskellPackages.nix
+++ nix/modifiedHaskellPackages.nix
@@ -1,25 +1,26 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler }:
 let
   beam = import ./beam.nix { inherit nixpkgs; };
-  monad-log = import ./monad-log.nix;
 in
-(if compiler == "default"
-then nixpkgs.haskell.packages.ghc802
-else nixpkgs.haskell.packages.${compiler}).override {
+  nixpkgs.haskell.packages.${compiler}.override {
   overrides = self: super: {
     beam-core =
       import ./beam-core.nix { haskellPackages = self; inherit beam; };
     beam-postgres =
       import ./beam-postgres.nix { haskellPackages = self; inherit beam; };
     beam-migrate =
-      import ./beam-migrate.nix { haskellPackages = self; inherit beam; };
-    monad-log = self.callPackage monad-log {};
-    # servant-auth-client is too new on 17.09
+      import ./beam-migrate.nix { haskellPackages = self; inherit beam; inherit nixpkgs; };
+    monad-log = self.callPackage ./monad-log.nix {};
+    # servant-auth-client in nixpkgs (17.09 and 18.03 at least) is too new for servant-0.11
     servant-auth-client =
-      if self.servant.version < "0.13"
-      then self.callPackage ./servant-auth-client-0.3.nix {}
-      else super.servant-auth-client;
+      # Tests fail on GHC 8.2.2 due to main module not being named "Main.hs"
+      nixpkgs.haskell.lib.dontCheck (self.callHackage "servant-auth-client" "0.3.0.0" {});
     # tests shell out to postgres exes that aren't on the path
     tmp-postgres = nixpkgs.haskell.lib.dontCheck super.tmp-postgres;
+    # concurrent-output depends on process >= 1.6, and GHC 8.0.2 comes with 1.4
+    concurrent-output =
+      if nixpkgs.haskell.compiler.${compiler}.version < "8.2"
+      then nixpkgs.haskell.lib.doJailbreak super.concurrent-output
+      else super.concurrent-output;
   };
 }
--- shell.nix
+++ shell.nix
@@ -1,10 +1,23 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{ nixpkgs ? "default", compiler ? "ghc802" }:
 let
+  systemNixpkgs = import <nixpkgs> { };
+  defaultNixpkgs = import (systemNixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "bca2ee28db44bc2d2d1b5e97866ca4052d88e94f";
+    sha256 = "0l9jpnkvjcj9p181ns9g4gm57n3ni4q07ynkh80r6d6qidw8cwnq";
+  }) {};
 
-  inherit (nixpkgs) pkgs;
+  pkgs =
+    if nixpkgs == null
+      then systemNixpkgs
+    else if nixpkgs == "default"
+      then defaultNixpkgs
+    else
+      nixpkgs;
 
   haskellPackages =
-    import ./nix/modifiedHaskellPackages.nix { inherit nixpkgs compiler; };
+    import ./nix/modifiedHaskellPackages.nix { nixpkgs = pkgs; inherit compiler; };
 
   drv =
     pkgs.haskell.lib.overrideCabal
@@ -18,6 +31,5 @@ let
               })
           ];
       });
-
 in
   if pkgs.lib.inNixShell then drv.env else drv
