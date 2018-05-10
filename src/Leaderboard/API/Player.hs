2f3ef64daa570dd077e6fe00555cc337a53b5f04
Andrew McCluskey <andrew@ajmccluskey.com>
2018-05-10 20:44:56 +1000

    Rejigger Player API
    I can't remember exactly what I did here. I think I pulled out some functions
    and generally made things a bit nicer and less repetitive.

--- src/Leaderboard/API/Player.hs
+++ src/Leaderboard/API/Player.hs
@@ -86,32 +86,11 @@ register jwts arp rp =
           throwError err401
         Right Player{..} ->
           if _playerIsAdmin
-            then (insertPlayer' jwts rp)
+            then (makeToken jwts <=< playerId <=< insertPlayer') rp
             else throwError $ err401 {errBody = "Must be an admin to register a new player"}
     ar ->  do
       Log.info $ "Failed authentication: " <> T.pack (show ar)
       throwError $ err401 {errBody = BSL8.pack (show ar)}
-
-insertPlayer'
-  :: ( HasDbConnPool r
-     , MonadBaseControl IO m
-     , MonadReader r m
-     , MonadError ServantErr m
-     , MonadLog Label m
-     )
-  => JWTSettings
-  -> RegisterPlayer
-  -> m Token
-insertPlayer' jwts rp = do
-  let
-    throwX msg = do
-      Log.error . (msg <>) . T.pack . show $ rp
-      throwError $ err500 {errBody = "Error registering player"}
-    throwNoPlayer = throwX "Inserting player into database failed -- `Nothing` returned for "
-    throwNoId = throwX "No player ID returned for "
-  mp <- withConn $ \conn -> liftIO $ insertPlayer conn rp
-  mpId <- maybe throwNoPlayer (pure . unAuto . _playerId) mp
-  maybe throwNoId (makeToken jwts) mpId
 
 registerFirst
   :: ( HasDbConnPool r
@@ -124,12 +103,17 @@ registerFirst
   -> JWTSettings
   -> RegisterPlayer
   -> m (AuthHeaders Token)
-registerFirst cs jwts rp = do
+registerFirst cs jwts rp =
+  withLabel (Label "/register-first") $ do
+  Log.info "registerFirst called"
+  let rp' = rp {_lbrIsAdmin = Just True}
   numPlayers' <- withConn $ liftIO . selectPlayerCount
   numPlayers <- either (const $ throwError err500) pure numPlayers'
   if numPlayers < 1
-    then addFirstPlayer cs jwts rp
-    else throwError $ err403 { errBody = "First user already added." }
+    then (authenticatePlayerId cs jwts <=< playerId <=< insertPlayer') rp'
+    else do
+      Log.error $ T.pack (show numPlayers) <> " players already registered"
+      throwError $ err403 { errBody = "First user already added." }
 
 authenticate
   :: ( HasDbConnPool r
@@ -154,7 +138,7 @@ authenticate cs jwts Login{..} =
     Left e           -> throwLoginFail e
     Right p@Player{..} ->
       if verifyPass' loginPass (EncryptedPass _playerPassword)
-        then acceptLogin' cs jwts p
+        then playerId p >>= authenticatePlayerId cs jwts
         else throwLoginFail ("Bad password" :: T.Text)
 
 playerCount
@@ -168,43 +152,52 @@ playerCount
 playerCount =
   withLabel (Label "/player-count") $ PlayerCount <$> getPlayerCount
 
-acceptLogin'
+authenticatePlayerId
   :: ( MonadError ServantErr m
      , MonadLog Label m
      )
   => CookieSettings
   -> JWTSettings
-  -> Player
+  -> Int
   -> m (AuthHeaders Token)
-acceptLogin' cs jwts Player{..} = do
-  let
-    throwNoId = do
-      Log.error ("Player with email '" <> _playerEmail <> "' missing id")
-      throwError err500
-  pId <- maybe throwNoId pure . unAuto $ _playerId
+authenticatePlayerId cs jwts pId = do
   token <- makeToken jwts pId
-  mApplyCookies <- liftIO . acceptLogin cs jwts . PlayerSession $ pId
-  case mApplyCookies of
-    Nothing           -> throwError err401
-    Just applyCookies -> pure $ applyCookies token
+  addAuthHeaders cs jwts pId token
 
-addFirstPlayer
-  :: ( MonadBaseControl IO m
-     , MonadError ServantErr m
-     , MonadReader r m
-     , HasDbConnPool r
+addAuthHeaders
+  :: ( MonadError ServantErr m
      , MonadLog Label m
      )
   => CookieSettings
   -> JWTSettings
-  -> RegisterPlayer
-  -> m (AuthHeaders Token)
-addFirstPlayer cs jwts rp = do
+  -> Int
+  -> response
+  -> m (AuthHeaders response)
+addAuthHeaders cs jwts pId r = do
+  mApplyCookies <- liftIO . acceptLogin cs jwts . PlayerSession $ pId
+  case mApplyCookies of
+    Nothing           -> throwError err401
+    Just applyCookies -> pure $ applyCookies r
+
+insertPlayer'
+  :: ( HasDbConnPool r
+     , MonadBaseControl IO m
+     , MonadReader r m
+     , MonadError ServantErr m
+     , MonadLog Label m
+     )
+  => RegisterPlayer
+  -> m Player
+insertPlayer' rp =
+  withLabel (Label "insertPlayer") $ do
+  Log.info $ "Inserting player: " <> (T.pack . show $ rp)
   let
-    playerThrow = throwError $ err500 { errBody = "User registration failed" }
-  -- Force admin flag to true for first registration
-  mp <- withConn $ \conn -> liftIO . insertPlayer conn $ rp {_lbrIsAdmin = Just True}
-  maybe playerThrow (acceptLogin' cs jwts) mp
+    throwX msg = do
+      Log.error . (msg <>) . T.pack . show $ rp
+      throwError $ err500 {errBody = "Error registering player"}
+    throwNoPlayer = throwX "Inserting player into database failed -- `Nothing` returned for "
+  mp <- withConn $ \conn -> liftIO $ insertPlayer conn rp
+  maybe throwNoPlayer pure mp
 
 getPlayerCount
   :: ( HasDbConnPool r
@@ -221,6 +214,20 @@ getPlayerCount = do
       throwError err500 { errBody = "Error retrieving player count" }
   en <- withConn $ liftIO . selectPlayerCount
   either throwNoPlayerCount pure en
+
+playerId
+  :: ( MonadError ServantErr m
+     , MonadLog Label m
+     )
+  => Player
+  -> m Int
+playerId Player{..} =
+  let
+    throwNoId = do
+      Log.error ("Player with email '" <> _playerEmail <> "' missing id")
+      throwError err500
+  in
+    maybe throwNoId pure . unAuto $ _playerId
 
 makeToken
   :: ( MonadError ServantErr m
