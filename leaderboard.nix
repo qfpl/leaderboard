397e7d997fbf4518e545f21d57e6b0a81ee66318
Andrew McCluskey <andrew@ajmccluskey.com>
2018-05-10 20:46:48 +1000

    Add deps needed for DB log file copy
    Forgot to include in earlier commit

--- leaderboard.nix
+++ leaderboard.nix
@@ -1,12 +1,12 @@
 { mkDerivation, aeson, base, beam-core, beam-migrate, beam-postgres
-, bytestring, connection, cryptonite, hedgehog, http-client-tls
-, http-types, jose, lens, mmorph, monad-control, monad-log, mtl
-, optparse-applicative, postgresql-simple, postgresql-simple-url
-, product-profunctors, resource-pool, retry, scrypt, servant
-, servant-auth, servant-auth-client, servant-auth-server
-, servant-client, servant-server, stdenv, tasty, tasty-hedgehog
-, text, tmp-postgres, transformers, transformers-base
-, uri-bytestring, warp, warp-tls
+, bytestring, connection, cryptonite, directory, filepath, hedgehog
+, http-client-tls, http-types, jose, lens, mmorph, monad-control
+, monad-log, mtl, optparse-applicative, postgresql-simple
+, postgresql-simple-url, product-profunctors, resource-pool, retry
+, scrypt, servant, servant-auth, servant-auth-client
+, servant-auth-server, servant-client, servant-server, stdenv
+, tasty, tasty-hedgehog, text, tmp-postgres, transformers
+, transformers-base, uri-bytestring, warp, warp-tls
 }:
 mkDerivation {
   pname = "leaderboard";
@@ -23,10 +23,10 @@ mkDerivation {
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
-    base bytestring connection hedgehog http-client-tls http-types lens
-    mmorph postgresql-simple postgresql-simple-url servant
-    servant-auth-client servant-client tasty tasty-hedgehog text
-    tmp-postgres transformers
+    base bytestring connection directory filepath hedgehog
+    http-client-tls http-types lens mmorph postgresql-simple
+    postgresql-simple-url servant servant-auth-client servant-client
+    tasty tasty-hedgehog text tmp-postgres transformers
   ];
   license = stdenv.lib.licenses.bsd3;
 }
