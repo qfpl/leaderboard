40d0d2aa25e0c3092ea40cb3ca3a054dddb4ca47
Andrew McCluskey <andrew@ajmccluskey.com>
2018-05-10 20:47:20 +1000

    Fix TestClient

--- test/Leaderboard/TestClient.hs
+++ test/Leaderboard/TestClient.hs
@@ -1,19 +1,26 @@
 module Leaderboard.TestClient where
 
 import           Data.ByteString        (ByteString)
-import           Servant.API            ((:<|>) ((:<|>)), NoContent)
-import qualified Servant.Auth.Client    as Client (Token)
+import           Servant.API            ((:<|>) ((:<|>)), NoContent,
+                                         getResponse)
+import qualified Servant.Auth.Client    as SAC (Token (Token))
 import           Servant.Client         (ClientM, client)
 
 import           Leaderboard.API        (LeaderboardAPI, leaderboardAPI)
 import           Leaderboard.API.Player (AuthHeaders)
 import           Leaderboard.Types      (Login, PlayerCount, RegisterPlayer,
-                                         Token)
+                                         Token (..))
 
-lcRegister      :: Client.Token -> RegisterPlayer -> ClientM Token
-lcRegisterFirst :: RegisterPlayer -> ClientM (AuthHeaders Token)
-lcAuthenticate  :: Login -> ClientM (AuthHeaders Token)
-lcPlayerCount   :: ClientM PlayerCount
+register      :: SAC.Token -> RegisterPlayer -> ClientM Token
+registerFirst :: RegisterPlayer -> ClientM (AuthHeaders Token)
+authenticate  :: Login -> ClientM (AuthHeaders Token)
+playerCount   :: ClientM PlayerCount
 
-(lcRegister :<|> lcRegisterFirst :<|> lcAuthenticate :<|> lcPlayerCount) =
+(register :<|> registerFirst :<|> authenticate :<|> playerCount) =
   client leaderboardAPI
+
+toServantToken
+  :: AuthHeaders Token
+  -> SAC.Token
+toServantToken =
+  SAC.Token . getToken . getResponse
