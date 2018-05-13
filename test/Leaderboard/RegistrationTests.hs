6612e9d134813910c879c273bd63c5afec9fc9a6
Andrew McCluskey <andrew@ajmccluskey.com>
2018-05-14 09:39:11 +1000

    Break some stuff out into `SharedState`
    `SharedState` is the cute module name I gave to the dumping ground for shared
    state machine components, such as our model state.

--- test/Leaderboard/RegistrationTests.hs
+++ test/Leaderboard/RegistrationTests.hs
@@ -1,51 +1,39 @@
-{-# LANGUAGE KindSignatures     #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE RecordWildCards    #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE KindSignatures    #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module Leaderboard.RegistrationTests
   ( registrationTests
   ) where
 
-import           Control.Monad.IO.Class    (MonadIO, liftIO)
+import           Control.Monad.IO.Class    (liftIO)
 import           Data.Bool                 (bool)
 import qualified Data.Map                  as M
 import           Data.Semigroup            ((<>))
 import qualified Data.Set                  as S
-import           Data.Text                 (Text)
 import           Network.HTTP.Types.Status (forbidden403)
 import           Servant.Auth.Client       (Token)
-import           Servant.Client            (ClientEnv, ClientM,
-                                            ServantError (..), runClientM)
+import           Servant.Client            (ClientEnv, ServantError (..))
 
 import           Hedgehog                  (Callback (..), Command (Command),
-                                            Eq1, Gen, HTraversable (htraverse),
-                                            PropertyT, Show1, Var (Var),
-                                            annotateShow, assert, concrete,
-                                            executeSequential, failure, forAll,
-                                            property, (===))
+                                            Gen, HTraversable (htraverse),
+                                            PropertyT, Var (Var), annotateShow,
+                                            assert, concrete, executeSequential,
+                                            failure, forAll, property, (===))
 import qualified Hedgehog.Gen              as Gen
 import qualified Hedgehog.Range            as Range
 
 import           Test.Tasty                (TestTree, testGroup)
 import           Test.Tasty.Hedgehog       (testProperty)
 
+import           Leaderboard.SharedState   (LeaderboardState (..), PlayerMap,
+                                            PlayerWithToken (..), failureClient,
+                                            genAdminToken, successClient)
 import           Leaderboard.TestClient    (fromLbToken, fromLbToken',
                                             getPlayerCount, register,
                                             registerFirst)
 import           Leaderboard.Types         (PlayerCount (..),
                                             RegisterPlayer (..))
-
-data PlayerWithToken v =
-  PlayerWithToken
-  { _pwtEmail    :: Text
-  , _pwtName     :: Text
-  , _pwtPassword :: Text
-  , _pwtIsAdmin  :: Maybe Bool
-  , _pwtToken    :: Var Token v
-  }
-  deriving (Eq, Show)
--- makeLenses ''PlayerWithToken
 
 registrationTests
   :: IO ()
@@ -57,54 +45,18 @@ registrationTests truncateTables env =
     , propRegister env truncateTables
     ]
 
-failureClient
-  :: MonadIO m
-  => (a -> String)
-  -> ClientEnv
-  -> ClientM a
-  -> m ServantError
-failureClient f ce a = do
-  r <- liftIO $ runClientM a ce
-  either pure (fail . f) r
-
-successClient
-  :: MonadIO m
-  => (ServantError -> String)
-  -> ClientEnv
-  -> ClientM a
-  -> m a
-successClient f ce a = do
-  r <- liftIO $ runClientM a ce
-  either (fail . f) pure r
-
-genNonEmptyUnicode
-  :: Gen Text
-genNonEmptyUnicode =
-  Gen.text (Range.linear 1 100) Gen.unicode
-
-genAdminToken
-  :: RegisterState v
-  -> Maybe (Gen (Var Token v))
-genAdminToken (RegisterState ps as) =
-  let
-    -- Emails in admin _must_ be a subset of those in players. Without a Traversable
-    -- instance for Gen I couldn't make this be not partial.
-    f = _pwtToken . (M.!) ps
-    mGEmail =  bool (pure . Gen.element . S.toList $ as) Nothing $ S.null as
-  in
-    fmap (fmap f) mGEmail
-
-type PlayerMap v = M.Map Text (PlayerWithToken v)
-
 genRegPlayerRandomAdmin
   :: PlayerMap v
   -> Gen RegisterPlayer
 genRegPlayerRandomAdmin ps =
-  LeaderboardRegistration
-    <$> Gen.filter (`S.notMember` M.keysSet ps) genNonEmptyUnicode
-    <*> genNonEmptyUnicode
-    <*> genNonEmptyUnicode
-    <*> Gen.maybe Gen.bool
+  let
+    genNonEmptyUnicode = Gen.text (Range.linear 1 100) Gen.unicode
+  in
+    LeaderboardRegistration
+      <$> Gen.filter (`S.notMember` M.keysSet ps) genNonEmptyUnicode
+      <*> genNonEmptyUnicode
+      <*> genNonEmptyUnicode
+      <*> Gen.maybe Gen.bool
 
 mkPlayerWithToken
   :: RegisterPlayer
@@ -119,10 +71,6 @@ mkPlayerWithToken LeaderboardRegistration{..} _pwtToken =
   in
     PlayerWithToken{..}
 
--- | Map emails to players and keep a set of admin emails
-data RegisterState (v :: * -> *) =
-  RegisterState (PlayerMap v) (S.Set Text)
-
 --------------------------------------------------------------------------------
 -- PLAYER COUNT
 --------------------------------------------------------------------------------
@@ -135,14 +83,14 @@ instance HTraversable GetPlayerCount where
 
 cGetPlayerCount
   :: ClientEnv
-  -> Command Gen (PropertyT IO) RegisterState
+  -> Command Gen (PropertyT IO) LeaderboardState
 cGetPlayerCount env =
   let
     gen _s = Just . pure $ GetPlayerCount
     exe _i = successClient show env $ unPlayerCount <$> getPlayerCount
   in
     Command gen exe [
-      Ensure $ \(RegisterState ps as) _sNew _i c -> do
+      Ensure $ \(LeaderboardState ps as) _sNew _i c -> do
         annotateShow ps
         annotateShow as
         length ps === fromIntegral c
@@ -152,8 +100,6 @@ cGetPlayerCount env =
 --------------------------------------------------------------------------------
 -- REGISTER FIRST
 --------------------------------------------------------------------------------
-deriving instance Show1 v => Show (RegisterState v)
-deriving instance Eq1 v => Eq (RegisterState v)
 
 newtype RegFirst (v :: * -> *) =
     RegFirst RegisterPlayer
@@ -169,34 +115,34 @@ instance HTraversable RegFirstForbidden where
 
 cRegFirst
   :: ClientEnv
-  -> Command Gen (PropertyT IO) RegisterState
+  -> Command Gen (PropertyT IO) LeaderboardState
 cRegFirst env =
   let
-    gen (RegisterState ps _as) =
+    gen (LeaderboardState ps _as) =
       bool Nothing (Just $ RegFirst <$> genRegPlayerRandomAdmin ps) $ null ps
     execute (RegFirst rp) =
       let mkError = (("Should succeed with token, but got: " <>) . show)
        in successClient mkError env . fmap fromLbToken' $ registerFirst rp
   in
     Command gen execute [
-      Require $ \(RegisterState ps _) _input -> null ps
+      Require $ \(LeaderboardState ps _) _input -> null ps
     , Update $ \_s (RegFirst lbr@LeaderboardRegistration{..}) t ->
-        RegisterState (M.singleton _lbrEmail (mkPlayerWithToken lbr t)) (S.singleton _lbrEmail)
-    , Ensure $ \_sOld (RegisterState psNew _) (RegFirst _rp) _t -> length psNew === 1
+        LeaderboardState (M.singleton _lbrEmail (mkPlayerWithToken lbr t)) (S.singleton _lbrEmail)
+    , Ensure $ \_sOld (LeaderboardState psNew _) (RegFirst _rp) _t -> length psNew === 1
     ]
 
 cRegFirstForbidden
   :: ClientEnv
-  -> Command Gen (PropertyT IO) RegisterState
+  -> Command Gen (PropertyT IO) LeaderboardState
 cRegFirstForbidden env =
   let
-    gen (RegisterState ps _as) =
+    gen (LeaderboardState ps _as) =
       bool (Just $ RegFirstForbidden <$> genRegPlayerRandomAdmin ps) Nothing $ null ps
     execute (RegFirstForbidden rp) =
       failureClient (const "Should return 403") env $ registerFirst rp
   in
     Command gen execute [
-      Require $ \(RegisterState ps _as) _input -> not (null ps)
+      Require $ \(LeaderboardState ps _as) _input -> not (null ps)
     , Ensure $ \sOld sNew _input se -> do
         sOld === sNew
         case se of
@@ -216,17 +162,17 @@ instance HTraversable Register where
 
 cRegister
   :: ClientEnv
-  -> Command Gen (PropertyT IO) RegisterState
+  -> Command Gen (PropertyT IO) LeaderboardState
 cRegister env =
   let
-    gen rs@(RegisterState ps _as) =
+    gen rs@(LeaderboardState ps _as) =
       (Register <$> genRegPlayerRandomAdmin ps <*>) <$> genAdminToken rs
     execute (Register rp token) =
       successClient show env . fmap fromLbToken $ register (concrete token) rp
   in
     Command gen execute [
-      Require $ \(RegisterState _ps as) _input -> not (null as)
-    , Update $ \(RegisterState ps as) (Register rp@LeaderboardRegistration{..} _rqToken) t ->
+      Require $ \(LeaderboardState _ps as) _input -> not (null as)
+    , Update $ \(LeaderboardState ps as) (Register rp@LeaderboardRegistration{..} _rqToken) t ->
         let
           newPlayers = M.insert _lbrEmail (mkPlayerWithToken rp t) ps
           newAdmins =
@@ -234,15 +180,15 @@ cRegister env =
               Just True -> S.insert _lbrEmail as
               _         -> as
         in
-          RegisterState newPlayers newAdmins
-    , Ensure $ \(RegisterState psOld _asOld) (RegisterState psNew _asNew) _input _output ->
+          LeaderboardState newPlayers newAdmins
+    , Ensure $ \(LeaderboardState psOld _asOld) (LeaderboardState psNew _asNew) _input _output ->
         length psNew === length psOld + 1
     ]
 
 initialState
-  :: RegisterState (v :: * -> *)
+  :: LeaderboardState (v :: * -> *)
 initialState =
-  RegisterState M.empty S.empty
+  LeaderboardState M.empty S.empty
 
 propRegFirst
   :: ClientEnv
---
+++ test/Leaderboard/SharedState.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE KindSignatures     #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+module Leaderboard.SharedState where
+
+import           Control.Monad.IO.Class    (MonadIO, liftIO)
+import           Data.Bool                 (bool)
+import qualified Data.Map                  as M
+import qualified Data.Set                  as S
+import           Data.Text                 (Text)
+import           Servant.Auth.Client       (Token)
+import           Servant.Client            (ClientEnv, ClientM,
+                                            ServantError (..), runClientM)
+
+import           Hedgehog                  (Gen, Var, Eq1, Show1)
+import qualified Hedgehog.Gen              as Gen
+
+-- | Map emails to players and keep a set of admin emails
+data LeaderboardState (v :: * -> *) =
+  LeaderboardState (PlayerMap v) (S.Set Text)
+deriving instance Show1 v => Show (LeaderboardState v)
+deriving instance Eq1 v => Eq (LeaderboardState v)
+
+type PlayerMap v = M.Map Text (PlayerWithToken v)
+
+data PlayerWithToken v =
+  PlayerWithToken
+  { _pwtEmail    :: Text
+  , _pwtName     :: Text
+  , _pwtPassword :: Text
+  , _pwtIsAdmin  :: Maybe Bool
+  , _pwtToken    :: Var Token v
+  }
+  deriving (Eq, Show)
+
+failureClient
+  :: MonadIO m
+  => (a -> String)
+  -> ClientEnv
+  -> ClientM a
+  -> m ServantError
+failureClient f ce a = do
+  r <- liftIO $ runClientM a ce
+  either pure (fail . f) r
+
+successClient
+  :: MonadIO m
+  => (ServantError -> String)
+  -> ClientEnv
+  -> ClientM a
+  -> m a
+successClient f ce a = do
+  r <- liftIO $ runClientM a ce
+  either (fail . f) pure r
+
+genAdminToken
+  :: LeaderboardState v
+  -> Maybe (Gen (Var Token v))
+genAdminToken (LeaderboardState ps as) =
+  let
+    -- Emails in admin _must_ be a subset of those in players. Without a Traversable
+    -- instance for Gen I couldn't make this be not partial.
+    f = _pwtToken . (M.!) ps
+    mGEmail =  bool (pure . Gen.element . S.toList $ as) Nothing $ S.null as
+  in
+    fmap (fmap f) mGEmail
